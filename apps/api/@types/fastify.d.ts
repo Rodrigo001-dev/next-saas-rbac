f6a0194419687caae5577fb230ef514546b36bfb
Rodrigo Rael <rodrigorael53@gmail.com>
2024-06-10 11:36:07 -0300

    :sparkles: feat: membership middleware & get membership route

--- apps/api/@types/fastify.d.ts
+++ apps/api/@types/fastify.d.ts
@@ -3,5 +3,8 @@ import 'fastify'
 declare module 'fastify' {
   export interface FastifyRequest {
     getCurrentUserId(): Promise<string>
+    getUserMembership(
+      slug: string,
+    ): Promise<{ organization: Organization; membership: Member }>
   }
 }
