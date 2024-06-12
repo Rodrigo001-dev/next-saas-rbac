b5390443eeb2f83641be518c3fde35fdd55099fc
Rodrigo Rael <rodrigorael53@gmail.com>
2024-06-12 11:27:26 -0300

    :sparkles: feat: organization CRUD

--- apps/api/src/http/routes/orgs/update-organization.ts
+++ apps/api/src/http/routes/orgs/update-organization.ts
@@ -1,44 +1,63 @@
+import { organizationSchema } from '@saas/auth'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
 import { auth } from '@/http/middlewares/auth'
 import { BadRequestError } from '@/http/routes/_errors/bad-request-error'
+import { UnauthorizedError } from '@/http/routes/_errors/unauthorized-error'
 import { prisma } from '@/lib/prisma'
-import { createSlug } from '@/utils/create-slug'
+import { getUserPermissions } from '@/utils/get-user-permissions'
 
-export async function createOrganization(app: FastifyInstance) {
+export async function updateOrganization(app: FastifyInstance) {
   app
     .withTypeProvider<ZodTypeProvider>()
     .register(auth)
-    .post(
-      '/organizations',
+    .put(
+      '/organizations/:slug',
       {
         schema: {
           tags: ['Organizations'],
-          summary: 'Create a new organization',
+          summary: 'Update organization details',
           security: [{ bearerAuth: [] }],
           body: z.object({
             name: z.string(),
             domain: z.string().nullish(),
             shouldAttachUsersByDomain: z.boolean().optional(),
           }),
+          params: z.object({
+            slug: z.string(),
+          }),
           response: {
-            201: z.object({
-              organizationId: z.string().uuid(),
-            }),
+            204: z.null(),
           },
         },
       },
       async (request, reply) => {
+        const { slug } = request.params
         const userId = await request.getCurrentUserId()
+        const { membership, organization } =
+          await request.getUserMembership(slug)
 
         const { name, domain, shouldAttachUsersByDomain } = request.body
 
+        const authOrganization = organizationSchema.parse(organization)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('update', authOrganization)) {
+          throw new UnauthorizedError(
+            `You're not allowed to update this organization.`,
+          )
+        }
+
         if (domain) {
-          const organizationByDomain = await prisma.organization.findUnique({
+          const organizationByDomain = await prisma.organization.findFirst({
             where: {
               domain,
+              id: {
+                not: organization.id,
+              },
             },
           })
 
@@ -49,25 +68,18 @@ export async function createOrganization(app: FastifyInstance) {
           }
         }
 
-        const organization = await prisma.organization.create({
+        await prisma.organization.update({
+          where: {
+            id: organization.id,
+          },
           data: {
             name,
-            slug: createSlug(name),
             domain,
             shouldAttachUsersByDomain,
-            ownerId: userId,
-            members: {
-              create: {
-                userId,
-                role: 'ADMIN',
-              },
-            },
           },
         })
 
-        return reply.status(201).send({
-          organizationId: organization.id,
-        })
+        return reply.status(204).send()
       },
     )
 }
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -19,6 +19,11 @@ import { requestPasswordRecover } from '@/http/routes/auth/request-password-reco
 import { resetPassword } from '@/http/routes/auth/reset-password'
 import { createOrganization } from '@/http/routes/orgs/create-organization'
 import { getMembership } from '@/http/routes/orgs/get-membership'
+import { getOrganization } from '@/http/routes/orgs/get-organization'
+import { getOrganizations } from '@/http/routes/orgs/get-organizations'
+import { shutdownOrganization } from '@/http/routes/orgs/shutdown-organization'
+import { transferOrganization } from '@/http/routes/orgs/transfer-organization'
+import { updateOrganization } from '@/http/routes/orgs/update-organization'
 
 import { createAccount } from './routes/auth/create-account'
 
@@ -68,6 +73,11 @@ app.register(resetPassword)
 
 app.register(createOrganization)
 app.register(getMembership)
+app.register(getOrganization)
+app.register(getOrganizations)
+app.register(updateOrganization)
+app.register(shutdownOrganization)
+app.register(transferOrganization)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
