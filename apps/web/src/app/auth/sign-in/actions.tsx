f00ce092548be504931e263e29ded838dce13efb
Rodrigo Rael <rodrigorael53@gmail.com>
2024-06-19 10:20:03 -0300

    :sparkles: feat: server actions & ky http client

---
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -0,0 +1,20 @@
+'use server'
+
+import { signInWithPassword } from '@/http/sign-in-with-password'
+
+export async function signInWithEmailAndPassword(
+  previousState: unknown,
+  data: FormData,
+) {
+  const { email, password } = Object.fromEntries(data)
+
+  await new Promise((resolve) => setTimeout(() => resolve, 2000))
+
+  const result = await signInWithPassword({
+    email: String(email),
+    password: String(password),
+  })
+
+  console.log({ result })
+  return 'Sucesso!'
+}
