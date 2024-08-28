a1aad7a1045d446e6bd97be3d768d340d71cb9c0
Rodrigo Rael <rodrigorael53@gmail.com>
2024-08-28 13:52:23 -0300

    :books: docs: update Gif

--- apps/api/src/http/routes/invites/accept-invite.ts
+++ apps/api/src/http/routes/invites/accept-invite.ts
@@ -11,7 +11,7 @@ export async function acceptInvite(app: FastifyInstance) {
     .withTypeProvider<ZodTypeProvider>()
     .register(auth)
     .post(
-      '/invites/:inviteId',
+      '/invites/:inviteId/accept',
       {
         schema: {
           tags: ['Invites'],
